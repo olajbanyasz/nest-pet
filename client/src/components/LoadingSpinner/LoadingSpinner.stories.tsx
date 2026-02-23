b6675089f526b85e87fc3855e60f6782cf7c35e2
Zsolt Andre <zsolt_andre@epam.com>
2026-02-23 14:56:25 +0100

    new story added to LoadingSpinner

---
+++ client/src/components/LoadingSpinner/LoadingSpinner.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
+import LoadingSpinner from './LoadingSpinner';
+
+const meta: Meta<typeof LoadingSpinner> = {
+  title: 'Components/LoadingSpinner/LoadingSpinner',
+  component: LoadingSpinner,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof LoadingSpinner>;
+
+export const Default: Story = {};
