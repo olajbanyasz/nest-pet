d4a1ff19d29fa375d9297b60bcf8420f1b4571df
Zsolt Andre <zsolt_andre@epam.com>
2026-02-23 13:05:30 +0100

    storybook style issue fix

--- client/.storybook/preview.ts
+++ client/.storybook/preview.ts
@@ -1,4 +1,5 @@
-import 'primereact/resources/themes/lara-light-blue/theme.css';
+import '../src/App.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 
